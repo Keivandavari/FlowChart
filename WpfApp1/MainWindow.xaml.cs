5eb6882ceb6629aa1713a820c3f6e0b82b0140e0
keivandavari <keivandavari@icloud.com>
2019-01-11 07:20:44 -0500

    goodmorning

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -40,6 +40,7 @@ namespace WpfApp1
 
         private void Add_Clicked(object sender, RoutedEventArgs e)
         {
+            int n = 0;
             MaxAvailableHeight = canva.ActualHeight;
             MaxAvailableWidth = canva.ActualWidth;
             fc.Add(inputsT.Text.Split(',').ToList().Select(t => t.Trim()).ToList(), outputT.Text, fT.Text);
@@ -48,6 +49,7 @@ namespace WpfApp1
 
         private void Draw()
         {
+            colorCounter = 0;
             canva.Children.Clear();
             int i = 0;
             for (i = 0; i < fc.chart.Count; i++)
@@ -84,9 +86,20 @@ namespace WpfApp1
 
             Rectangle rect = new System.Windows.Shapes.Rectangle();
             Ellipse ellipse = new Ellipse();
+  
 
             if (j % 2 == 0)
             {
+                //List<Point> points = new List<Point>();
+                //points.Add(new Point((canva.ActualWidth / stacks) * (j) + (canva.ActualWidth / stacks) / 4,
+                //                     (canva.ActualHeight / slices1) * (k) + topAddOn(j)));
+                //points.Add(new Point((canva.ActualWidth / stacks) * (j),
+                //                     (canva.ActualHeight / slices1) * (k) + topAddOn(j) + (canva.ActualHeight / slices1) / 4));
+                //points.Add(new Point((canva.ActualWidth / stacks) * (j) + (canva.ActualWidth / stacks) / 4,
+                //                     (canva.ActualHeight / slices1) * (k) + topAddOn(j) + (canva.ActualHeight / slices1) / 2));
+                //points.Add(new Point((canva.ActualWidth / stacks) * (j) + (canva.ActualWidth / stacks) / 2,
+                //     (canva.ActualHeight / slices1) * (k) + topAddOn(j) + (canva.ActualHeight / slices1) / 4));
+                //MakeCubicCurve(points.ToArray());
                 rect = new Rectangle()
                 {
                     Name = fc.chart[j][k][0].ToString(),
@@ -97,8 +110,8 @@ namespace WpfApp1
                     Stroke = Brushes.Black,
                     StrokeThickness = 1,
                 };
-                canva.Children.Add(rect);
-                rect.Margin = new Thickness((canva.ActualWidth / stacks) * (j), (canva.ActualHeight / slices1) * (k) + topAddOn(j), 0, 0);
+                //canva.Children.Add(rect);
+                //rect.Margin = new Thickness((canva.ActualWidth / stacks) * (j), (canva.ActualHeight / slices1) * (k) + topAddOn(j), 0, 0);
             }
             else
             {
@@ -112,8 +125,8 @@ namespace WpfApp1
                     Stroke = Brushes.Black,
                     StrokeThickness = 1,
                 };
-                canva.Children.Add(ellipse);
-                ellipse.Margin = new Thickness((canva.ActualWidth / stacks) * (j), (canva.ActualHeight / slices1) * (k) + topAddOn(j), 0, 0);
+                //canva.Children.Add(ellipse);
+                //ellipse.Margin = new Thickness((canva.ActualWidth / stacks) * (j), (canva.ActualHeight / slices1) * (k) + topAddOn(j), 0, 0);
             }
 
             var textBlock = new TextBlock()
@@ -122,6 +135,11 @@ namespace WpfApp1
                 Text = j % 2 == 1 ? fc.chart[j][k][fc.chart[j][k].Count - 2].ToString() : fc.chart[j][k][0].ToString(),
                 VerticalAlignment = VerticalAlignment.Top,
                 HorizontalAlignment = HorizontalAlignment.Left,
+                Foreground = j%2==0 ? (SolidColorBrush)(new BrushConverter().ConvertFrom("#3E50B4")):
+                                       (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF3F80")),
+                FontFamily = new System.Windows.Media.FontFamily("SegoeUI"),
+                FontWeight = FontWeights.Light,
+                FontSize = 24,
             };
             canva.Children.Add(textBlock);
             textBlock.Margin = new Thickness((canva.ActualWidth / stacks) * (j) + (canva.ActualWidth / stacks) / 5 - textBlock.ActualWidth, (canva.ActualHeight / slices1) * (k) + (canva.ActualHeight / slices1) / 6 + topAddOn(j), 0, 0);
@@ -151,16 +169,20 @@ namespace WpfApp1
 
         private void Drawlines(int j, int k, int x, int y, int z, int stacks)
         {
-            colorCounter++;
-            List<KnownColor> colors = Enum.GetValues(typeof(KnownColor))
-                                      .Cast<KnownColor>()
-                                      .ToList();
-            Color color = new Color();
-            color = Color.FromName(colors[colorCounter].ToString());
+            //colorCounter++;
+            //List<KnownColor> colors = Enum.GetValues(typeof(KnownColor))
+                                      //.Cast<KnownColor>()
+                                      //.ToList();
+            //Color color = new Color();
+            //color = Color.FromName(colors[colorCounter].ToString());
             Random rand = new Random();
             int distance = y - j,numOfInputs=0,originalNumOfInputs=0;
             double nextHeight = 0;
             List<Point> points = new List<Point>();
+            points.Add(new Point((canva.ActualWidth / stacks) * (j) + (canva.ActualWidth / stacks) / 8,
+            (canva.ActualHeight / slices(j)) * (k) + topAddOn(j) +(canva.ActualWidth / stacks)/4+ (canva.ActualHeight / slices(j)) / 2 * x / (fc.chart[j][k].Count)));
+            points.Add(new Point((canva.ActualWidth / stacks) * (j) + (canva.ActualWidth / stacks)/4,
+                    (canva.ActualHeight / slices(j)) * (k) + topAddOn(j) + (canva.ActualHeight / slices(j))/4 + (canva.ActualHeight / slices(j)) / 2 * x / (fc.chart[j][k].Count)));
             points.Add(new Point((canva.ActualWidth / stacks) * (j) + (canva.ActualWidth / stacks) / 2,
                      (canva.ActualHeight / slices(j)) * (k) + topAddOn(j) + (canva.ActualHeight / slices(j)) / 2 * x / (fc.chart[j][k].Count)));
             points.Add(new Point((canva.ActualWidth / stacks) * (j) + (canva.ActualWidth / stacks) / 2,
@@ -170,6 +192,7 @@ namespace WpfApp1
 
                 for (int i = j; i < y; i++)
                 {
+                    if (i > j) colorCounter--;
                     if (i == y - 1)
                     {
                         if (y % 2 == 0)
@@ -211,6 +234,11 @@ namespace WpfApp1
                             points.Add(new Point((canva.ActualWidth / stacks) * ((y)),
                             (canva.ActualHeight / slices(y)) * (z) + topAddOn(y) + (canva.ActualHeight / slices(y)) / 4));
                         }
+                        points.Add(new Point((canva.ActualWidth / stacks) * (y) + (canva.ActualWidth / stacks)/4,
+                            (canva.ActualHeight / slices(y)) * (z) + topAddOn(y)));
+                        //points.Add(new Point((canva.ActualWidth / stacks) * (y) + (canva.ActualWidth / stacks) / 2,
+                        //    (canva.ActualHeight / slices(y)) * (z) + topAddOn(y) + (canva.ActualHeight / slices(y))/4));
+
                     }
                     else
                     {
@@ -234,17 +262,8 @@ namespace WpfApp1
                         points.Add(new Point(points[3].X + (canva.ActualWidth / stacks) * 1 / 2,
                         nextHeight));
                     }
-                    System.Windows.Media.Color color1 = System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
-                    ////var b = GetBezierApproximation(points.ToArray(), 256);
-                    PathFigure pf = new PathFigure(b.Points[0], new[] { b }, false);
-                    PathFigureCollection pfc = new PathFigureCollection();
-                    pfc.Add(pf);
-                    var pge = new PathGeometry();
-                    pge.Figures = pfc;
-                    Path p = new Path();
-                    p.Data = pge;
-                    p.Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 0, 0));
-                    canva.Children.Add(p);
+                    //System.Windows.Media.Color color1 = System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+                    MakeCubicCurve(points.ToArray());
                     //for (int v = 0; v < points.Count - 1; v++)
                     //{
 
@@ -268,7 +287,24 @@ namespace WpfApp1
             }
         }
 
+        private void MakeCubicCurve(Point[] points)
+        {
+            if (colorCounter > indexcolors.Count() - 1) colorCounter = 0;
+            var b = GetBezierApproximation(points, 256);
+            PathFigure pf = new PathFigure(b.Points[0], new[] { b }, false);
+            PathFigureCollection pfc = new PathFigureCollection();
+            pfc.Add(pf);
+            var pge = new PathGeometry();
+            pge.Figures = pfc;
+            Path p = new Path();
+            p.Data = pge;
+            p.Stroke = (SolidColorBrush)(new BrushConverter().ConvertFrom(indexcolors[++colorCounter]));
+            canva.Children.Add(p);
+        }
 
+
+
+        // helpers
         PolyLineSegment GetBezierApproximation(Point[] controlPoints, int outputSegmentCount)
         {
             Point[] points = new Point[outputSegmentCount + 1];
@@ -288,13 +324,57 @@ namespace WpfApp1
             var P1 = GetBezierPoint(t, controlPoints, index + 1, count - 1);
             return new Point((1 - t) * P0.X + t * P1.X, (1 - t) * P0.Y + t * P1.Y);
         }
-
-
-
-
         private void In_Focused(object sender, RoutedEventArgs e)
         {
             inputsT.SelectAll();
+        }
+
+        private static String[] indexcolors = new String[]{
+
+        "#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
+
+        "#FFDBE5", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
+        "#5A0007", "#809693", "#FEFFE6", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+        "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
+        "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+        "#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+        "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
+        "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
+        
+        "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
+        "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+        "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
+        "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+        "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C",
+        "#83AB58", "#001C1E", "#D1F7CE", "#004B28", "#C8D0F6", "#A3A489", "#806C66", "#222800",
+        "#BF5650", "#E83000", "#66796D", "#DA007C", "#FF1A59", "#8ADBB4", "#1E0200", "#5B4E51",
+        "#C895C5", "#320033", "#FF6832", "#66E1D3", "#CFCDAC", "#D0AC94", "#7ED379", "#012C58",
+        
+        "#7A7BFF", "#D68E01", "#353339", "#78AFA1", "#FEB2C6", "#75797C", "#837393", "#943A4D",
+        "#B5F4FF", "#D2DCD5", "#9556BD", "#6A714A", "#001325", "#02525F", "#0AA3F7", "#E98176",
+        "#DBD5DD", "#5EBCD1", "#3D4F44", "#7E6405", "#02684E", "#962B75", "#8D8546", "#9695C5",
+        "#E773CE", "#D86A78", "#3E89BE", "#CA834E", "#518A87", "#5B113C", "#55813B", "#E704C4",
+        "#00005F", "#A97399", "#4B8160", "#59738A", "#FF5DA7", "#F7C9BF", "#643127", "#513A01",
+        "#6B94AA", "#51A058", "#A45B02", "#1D1702", "#E20027", "#E7AB63", "#4C6001", "#9C6966",
+        "#64547B", "#97979E", "#006A66", "#391406", "#F4D749", "#0045D2", "#006C31", "#DDB6D0",
+        "#7C6571", "#9FB2A4", "#00D891", "#15A08A", "#BC65E9", "#FFFFFE", "#C6DC99", "#203B3C",
+
+        "#671190", "#6B3A64", "#F5E1FF", "#FFA0F2", "#CCAA35", "#374527", "#8BB400", "#797868",
+        "#C6005A", "#3B000A", "#C86240", "#29607C", "#402334", "#7D5A44", "#CCB87C", "#B88183",
+        "#AA5199", "#B5D6C3", "#A38469", "#9F94F0", "#A74571", "#B894A6", "#71BB8C", "#00B433",
+        "#789EC9", "#6D80BA", "#953F00", "#5EFF03", "#E4FFFC", "#1BE177", "#BCB1E5", "#76912F",
+        "#003109", "#0060CD", "#D20096", "#895563", "#29201D", "#5B3213", "#A76F42", "#89412E",
+        "#1A3A2A", "#494B5A", "#A88C85", "#F4ABAA", "#A3F3AB", "#00C6C8", "#EA8B66", "#958A9F",
+        "#BDC9D2", "#9FA064", "#BE4700", "#658188", "#83A485", "#453C23", "#47675D", "#3A3F00",
+        "#061203", "#DFFB71", "#868E7E", "#98D058", "#6C8F7D", "#D7BFC2", "#3C3E6E", "#D83D66",
+        
+        "#2F5D9B", "#6C5E46", "#D25B88", "#5B656C", "#00B57F", "#545C46", "#866097", "#365D25",
+        "#252F99", "#00CCFF", "#674E60", "#FC009C", "#92896B"
+    };
+
+        private void Close_Clicked(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Close();
         }
     }
 
